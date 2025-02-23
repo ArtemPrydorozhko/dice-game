88dec008b665fcb3110780bbfb5b13a44dc51483
Artem Prydorozhko <43318829+ArtemPrydorozhko@users.noreply.github.com>
2025-02-23 13:04:15 +0200

    fix Debug singleton instantiation

--- src/game/utils/Debug.js
+++ src/game/utils/Debug.js
@@ -1,17 +1,16 @@
 import GUI from 'lil-gui';
 
 class Debug {
-  instance = null;
   constructor() {
     this.active = window.location.hash === '#debug';
     this.ui = this.active ? new GUI() : null;
   }
 
   static getInstance() {
-    if (!this.instance) {
-      this.instance = new Debug();
+    if (!Debug.instance) {
+      Debug.instance = new Debug();
     }
-    return this.instance;
+    return Debug.instance;
   }
 }
 
